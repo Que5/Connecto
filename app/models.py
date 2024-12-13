ac57ab5fbfa8fd9cdc4957e9895e359b59d57ec2
Que5 <phumlanibrightdlamini@gmail.com>
2024-12-13 16:57:31 +0200

    Model created

--- app/models.py
+++ app/models.py
@@ -1,4 +1,24 @@
 from django.db import models
+from django.contrib.auth import AbstractUser
 
 # Create your models here.
+
+class UserProfile(AbstractUser):
+    pass
+
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField(blank=True, default="")
+    word_count = models.IntegerField()
+    twitter_post = models.TextField(blank=True, default="")
+    status = models.CharField(max_length=20, 
+                                choices=(
+                                  ("draft", "Draft"),
+                                  ("in progress", "in progress"), 
+                                  ("published", "published"),
+                                    ),
+                                default="draft"
+                            )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
