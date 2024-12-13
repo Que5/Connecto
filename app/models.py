a79bc4e7421b9b6571942702d519df74b551aac6
Que5 <phumlanibrightdlamini@gmail.com>
2024-12-13 16:58:26 +0200

    Added the magic string

--- app/models.py
+++ app/models.py
@@ -22,3 +22,6 @@ class Article(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return self.title
+
