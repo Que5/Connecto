28575062527121ac2d6f36d940f23646eb4a4ad3
Que5 <phumlanibrightdlamini@gmail.com>
2024-12-14 10:35:20 +0200

    Form ammended

--- app/forms.py
+++ app/forms.py
@@ -1,13 +1,7 @@
 from django import forms
+from .models import Article
 
-class CreateArticleForm(forms.Form):
-    ARTICLE_STATUS = (
-        ("draft", "Draft"),
-        ("in progress", "In progress"),
-        ("published", "Published"),
-    )
-
-    title = forms.CharField(max_length=200)
-    status = forms.ChoiceField(choices=ARTICLE_STATUS)
-    content = forms.CharField(widget=forms.Textarea)
-    word_count = forms.IntegerField(widget=forms.Textarea, required=False)
+class CreateArticleForm(forms.ModelForm):
+    class Meta:
+        model = Article
+        fields = ("title", "status", "content", "word_count", "twitter_post")
