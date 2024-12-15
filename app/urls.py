b9a3183215f98bdf98455fe8c5c795e1c6c1773b
Que5 <phumlanibrightdlamini@gmail.com>
2024-12-15 12:25:50 +0200

    Cleaned up the urls by getting rid of "articles"

--- app/urls.py
+++ app/urls.py
@@ -3,8 +3,8 @@ from .views import ArticleCreateView, ArticleListView, ArticleDetailView, Articl
 
 urlpatterns = [
     path("", ArticleListView.as_view(), name="home"),
-    path("article/create/", ArticleCreateView.as_view(), name="create"),
-    path("article/<int:pk>/detail/", ArticleDetailView.as_view(), name="detail"),
-    path("article/<int:pk>/update/", ArticleUpdateView.as_view(), name="update"),
-    path("article/<int:pk>/delete/", ArticleDeleteView.as_view(), name="delete"),
+    path("create/", ArticleCreateView.as_view(), name="create"),
+    path("<int:pk>/detail/", ArticleDetailView.as_view(), name="detail"),
+    path("<int:pk>/update/", ArticleUpdateView.as_view(), name="update"),
+    path("<int:pk>/delete/", ArticleDeleteView.as_view(), name="delete"),
 ]
