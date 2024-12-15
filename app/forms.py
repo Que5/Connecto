cecbc106b6c5ea897678ebfe190c37cc47fdc602
Que5 <phumlanibrightdlamini@gmail.com>
2024-12-15 11:10:29 +0200

    Deleted forms file, imported reverse lazy, model and created an ArticleCreateView

--- app/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Article
-
-class CreateArticleForm(forms.ModelForm):
-    class Meta:
-        model = Article
-        fields = ("title", "status", "content", "word_count", "twitter_post")
--- app/views.py
+++ app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from .models import Article
+from django.views.generic import CreateView
+from django.urls import reverse_lazy 
 # Create your views here.
+
+class ArticleCreateView(CreateView):
+    model = Article
+    fields = "__all__"
+    success_url = "home"
+    template_name = "article_create.html"
+
