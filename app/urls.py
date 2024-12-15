2c784d1ee4c92be3d238e42dd044a1f999a9623d
Que5 <phumlanibrightdlamini@gmail.com>
2024-12-15 11:35:11 +0200

    Chnaged name from list to home

--- app/urls.py
+++ app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import ArticleCreateView, ArticleListView
 
 urlpatterns = [
-    path("", ArticleListView.as_view(), name="list"),
+    path("", ArticleListView.as_view(), name="home"),
     path("create/", ArticleCreateView.as_view(), name="create"),
 
 ]
