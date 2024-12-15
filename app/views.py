f965b0e6a58317ed062d154412bbd624a66d28d4
Que5 <phumlanibrightdlamini@gmail.com>
2024-12-15 11:59:15 +0200

    Added the remaining views

--- app/views.py
+++ app/views.py
@@ -1,18 +1,38 @@
 from django.shortcuts import render
 from .models import Article
-from django.views.generic import CreateView, ListView
+from django.views.generic import CreateView, ListView, UpdateView, DeleteView, DetailView
 from django.urls import reverse_lazy 
 # Create your views here.
+
+
+class ArticleListView(ListView):
+    model = Article
+    template_name = "app/article_list.html"
+
 
 class ArticleCreateView(CreateView):
     model = Article
     fields = "__all__"
     success_url = reverse_lazy("home")
-    template_name = "app/article_create.html"
+    template_name = "app/home.html"
 
-class ArticleListView(ListView):
+
+class ArticleDetailView(DetailView):
     model = Article
-    template_name = "app/article_list.html"
+    template_name = "app/article_detail.html"
+
+
+class ArticleUpdateView(UpdateView):
+    model = Article
+    fields = "__all__"
+    template_name = "app/article_update.html"
+    success_url = reverse_lazy("home")
+
+
+class ArticleDelete(DetailView):
+    model = Article
+    template_name = "app/article_delete.html"
+    success_url = reverse_lazy("home")
 
 
 
