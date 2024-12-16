41d688c27bf100bd22f846fb728b46eec62b27d3
Que5 <phumlanibrightdlamini@gmail.com>
2024-12-16 12:22:09 +0200

    Dashboard customized

--- app/admin.py
+++ app/admin.py
@@ -2,6 +2,14 @@ from django.contrib import admin
 from .models import UserProfile, Article
 
 # Register your models here.
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ["title", "word_count", "status", "created_at"]
+    list_filter = ["status"]
+    search_fields = ["title", "content"]
+    date_hierarchy = "created_at"
+    ordering =["created_at"]
+    readonly_fields = ["word_count", "created_at", "updated_at"]
+
 
 admin.site.register(UserProfile)
 admin.site.register(Article)
