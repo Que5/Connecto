483ebc6aed1e1e61f3eb74106fcb4256bc0bdfc9
Que5 <phumlanibrightdlamini@gmail.com>
2024-12-18 10:21:01 +0200

    Added Creator in the Article model

--- app/models.py
+++ app/models.py
@@ -1,5 +1,7 @@
 import re
+
 from django.db import models
+from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
@@ -25,6 +27,7 @@ class Article(models.Model):
                             )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    creator = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="articles")
 
     def __str__(self):
         return self.title
