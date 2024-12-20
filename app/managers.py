e0c9eb3705112a128663b545b6cb351d43ca4deb
Que5 <phumlanibrightdlamini@gmail.com>
2024-12-20 09:12:12 +0200

    managers file created

---
+++ app/managers.py
@@ -0,0 +1,9 @@
+from django.contrib.auth.models import BaseUserManager
+
+
+class UserProfileManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("The Email must be set")
+        email = self.normalize_email(email)
+        
