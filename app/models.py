f51a4de4ed0278f2cea36f9091a1036fa2c85799
Que5 <phumlanibrightdlamini@gmail.com>
2024-12-20 09:43:32 +0200

    Added user agregrate counts

--- app/models.py
+++ app/models.py
@@ -18,8 +18,17 @@ class UserProfile(AbstractUser):
     email = models.EmailField(max_length=255, unique=True)
 
     objects = UserProfileManager()
+
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
+
+    @property
+    def article_count(self):
+        return self.articles.count()
+    
+    @property
+    def written_words(self):
+        return self.articles.aggregate(models.Sum("word_count"))["word_count__sum"] or 0
 
 class Article(models.Model):                                                               
     title = models.CharField(max_length=100)
