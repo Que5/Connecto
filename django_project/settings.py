9e72beed48dd630d4534884f18541231f3433467
Que5 <phumlanibrightdlamini@gmail.com>
2024-12-22 18:27:55 +0200

    Added static settings

--- django_project/settings.py
+++ django_project/settings.py
@@ -156,6 +156,10 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
