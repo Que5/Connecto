22e5a8fb4d26332bbf5e776e52637f54c530efef
Que5 <phumlanibrightdlamini@gmail.com>
2024-12-22 12:48:11 +0200

    Connected PostreSQL database

--- django_project/settings.py
+++ django_project/settings.py
@@ -96,8 +96,13 @@ WSGI_APPLICATION = "django_project.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": config('NAME_DB'),
+        "USER": config('USER_DB'),
+        "PASSWORD": config('PASSWORD_DB'),
+        "HOST": config('HOST_DB'),
+        "PORT": config('PORT_DB')
+
     }
 }
 
